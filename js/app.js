003cf748f1dfe3e9e314a706c27fdbb40624c0b2
Jonathan Triana <jonfertri357@gmail.com>
2020-09-11 21:26:56 -0500

    Was added the option single player, with first move response

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ class TicTacToe {
 			[0, 4, 8],
 			[2, 4, 6],
 		];
+		this.corners = [0, 2, 6, 8];
+		this.centerOfLine = [1, 3, 5, 7];
+		this.singlePlayer = true;
 		this.addButtonEvent();
 	}
 
@@ -19,17 +22,29 @@ class TicTacToe {
 		this.optionsSelectedPlayer2 = [];
 		this.player = 0;
 		const selectedFunction = (e) => {
-			if (this.player % 2 == 0) {
-				e.target.innerText = "X";
-				this.optionsSelectedPlayer1.push(parseInt(e.target.value));
+			if (this.singlePlayer) {
+				if (this.player % 2 == 0) {
+					e.target.innerText = "x";
+					this.optionsSelectedPlayer1.push(parseInt(e.target.value));
+				} else {
+					this.automaticMove();
+				}
 			} else {
-				e.target.innerText = "O";
-				this.optionsSelectedPlayer2.push(parseInt(e.target.value));
+				if (this.player % 2 == 0) {
+					e.target.innerText = "X";
+					this.optionsSelectedPlayer1.push(parseInt(e.target.value));
+				} else {
+					e.target.innerText = "O";
+					this.optionsSelectedPlayer2.push(parseInt(e.target.value));
+				}
+				e.target.removeEventListener("click", selectedFunction);
 			}
-			e.target.removeEventListener("click", selectedFunction);
 			this.player++;
 			console.log(this.player);
 			this.checkIfWin();
+			if (this.player % 2 == 1) {
+				selectedFunction();
+			}
 		};
 		this.buttons.forEach((button) => {
 			button.addEventListener("click", selectedFunction);
@@ -59,6 +74,46 @@ class TicTacToe {
 				});
 			});
 		});
+	}
+
+	automaticMove() {
+		if (this.player == 1) {
+			if (this.optionsSelectedPlayer1[0] == 4) {
+				const randomNumber = Math.random();
+				console.log(randomNumber);
+				for (let i = 0; i <= 3; i++) {
+					if (randomNumber <= 0.25 * (i + 1)) {
+						this.numberSelected = this.corners[i];
+						break;
+					} else {
+						if (i == 3) {
+							console.log("You have an error in the Math.random");
+						}
+					}
+				}
+				console.log(this.numberSelected);
+				this.optionsSelectedPlayer2.push(this.numberSelected);
+
+				this.optionsToWin.forEach((singleOption) => {
+					singleOption.forEach((singleNumber) => {
+						if (singleNumber == this.numberSelected) {
+							this.buttons[this.numberSelected].innerText = "O";
+							this.buttons[this.numberSelected].removeEventListener(
+								"click",
+								this.addButtonEvent.selectedFunction
+							);
+						}
+					});
+				});
+			} else {
+				this.optionsSelectedPlayer2.push(4);
+				this.buttons[4].innerText = "O";
+				this.buttons[4].removeEventListener(
+					"click",
+					this.addButtonEvent.selectedFunction
+				);
+			}
+		}
 	}
 }
 
